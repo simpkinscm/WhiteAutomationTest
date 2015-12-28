82db0e90ed9f35e972b2f3c0e3ff432adc4eeb55
michael.simpkins <michael.simpkins@orasi.com>
2015-12-28 17:11:35 -0500

    Initial Buildout.

---
+++ UnitTestProject2/MyTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using TestStack.White.Factory;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.ListBoxItems;
+
+namespace WhiteFrameworkSandbox
+{
+    [TestFixture]
+    public class MyTests{
+        Window window;
+
+        [SetUp]
+        public void LaunchandLogin(){
+            //Launch App
+            const string exePath = @"C:\Users\michael.simpkins\workspace\Mercury Tours App\samples\Flights Application\FlightsGUI.exe";
+            var exeProcess = new ProcessStartInfo(exePath);
+            Application application = Application.AttachOrLaunch(exeProcess);
+
+            //Assign Login Window
+            window = application.GetWindow(SearchCriteria.ByText("HP MyFlight Sample Application"), InitializeOption.NoCache);
+
+            //Login
+            window.Get<TextBox>("agentName").SetValue("John");
+            window.Get<TextBox>("password").SetValue("HP");
+            window.Get<Button>("okButton").Click();
+
+            //Assign App Window
+            window = application.GetWindow(SearchCriteria.ByText("HP MyFlight Sample Application"), InitializeOption.NoCache);
+        }
+
+        [TearDown]
+        public void CloseApp(){
+            //Close App
+            window.Close();
+        }
+
+        [Test]
+        public void SearchandBookFlight() {
+            //search
+            window.Get<ComboBox>("fromCity").Items.Select("Denver");
+            window.Get<ComboBox>("toCity").Items.Select("Portland");
+            window.Get<DateTimePicker>("datePicker").SetDate(new System.DateTime(2016, 1, 26), DateFormat.MonthDayYear);
+            window.Get<ComboBox>("Class").Items.Select("Business");
+            window.Get<Button>(SearchCriteria.ByText("FIND FLIGHTS")).Click();
+            //results
+            ListView results = window.Get<ListView>("flightsDataGrid");
+            results.Rows[0].Cells[0].Click();
+            window.Get<Button>("selectFlightBtn").Click();
+            //book
+            window.Get<TextBox>("passengerName").SetValue("Mike Hunt");
+            window.Get<Button>("orderBtn").Click();
+            //Verify
+            ProgressBar progBar = window.Get<ProgressBar>("progBar");
+            do
+            {
+                System.Threading.Thread.Sleep(500);
+            } while (0 < progBar.Value && progBar.Value < 100);
+            Assert.True(window.Get<Label>("orderCompleted").Visible);
+        }
+    }
+}
